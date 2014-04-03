ce4b535b351dc232a557bd2a9d03e21622842ba3
asyncee <asyncee@users.noreply.github.com>
2014-04-03 12:34:48 +0400

    Updated sampleproject

--- sampleproject/demoapp/admin.py
+++ sampleproject/demoapp/admin.py
@@ -3,7 +3,7 @@ from django import forms
 
 from easy_select2 import select2_modelform_meta, Select2TextInput
 
-from .models import Tag, Note, MarkupType
+from .models import Note, Category
 
 
 class NoteAdminForm(forms.ModelForm):
@@ -11,27 +11,33 @@ class NoteAdminForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(NoteAdminForm, self).__init__(*args, **kwargs)
-        markup_data = list(MarkupType.objects.all().values('id', 'text'))
-        # dynamically override widget with existing markup choices
-        self.fields['markup'].widget = Select2TextInput(select2attrs={'data': markup_data})
 
-    def clean(self):
-        # process dirty data and fill cleaned_data with correct markup id
-        dirty_markup = self.data['markup']
-        try:
-            markup = MarkupType.objects.get(id=int(dirty_markup))
-        except (ValueError, MarkupType.DoesNotExist):
-            markup = MarkupType.objects.create(text=dirty_markup)
+        #
+        # Let's create the *current* list of choices for the mood field. By
+        # putting this code here, it will ensure that the list is up-to-date
+        # for each usage of the form.
+        #
+        # Also, you could personalize the experience by filtering the query to
+        # only those Notes previously edited by this user. The effect would be
+        # that each project Admin would see only their personal list of moods
+        # that they've used in the past.
+        #
+        # You should note, that we are using denormalized table
+        # structure there, what perfectly solves the problem.
+        #
+        mood_data = Note.objects.values_list('mood', flat=True).order_by('mood')
 
-        cleaned_data = super(NoteAdminForm, self).clean()
-        self.cleaned_data['markup'] = markup.id
-        return cleaned_data
+        self.fields['mood'].widget = Select2TextInput(select2attrs={
+            #
+            # This is how Select2 expects the list of choices.
+            #
+            'data': [{'id': x, 'text': x} for x in mood_data]
+        })
 
 
 class NoteAdmin(admin.ModelAdmin):
     form = NoteAdminForm
 
 
-admin.site.register(Tag)
-admin.site.register(MarkupType)
+admin.site.register(Category)
 admin.site.register(Note, NoteAdmin)
