2d9cd0da74272c8756cf844ba876eb2c908976da
asyncee <asyncee@users.noreply.github.com>
2014-09-28 17:50:04 +0400

    Added test models

---
+++ easy_select2/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Related(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+
+
+class TestFieldsModel(models.Model):
+    CHOICES = ((0, 'Zero'), (1, 'One'))
+    choice_field = models.IntegerField(choices=CHOICES)
+    fk_field = models.ForeignKey(Related, related_name='+')
+    m2m_field = models.ManyToManyField(Related, related_name='+')
+
+    class Meta:
+        managed = False
+
+
+class EmptyModel(models.Model):
+
+    class Meta:
+        managed = False
