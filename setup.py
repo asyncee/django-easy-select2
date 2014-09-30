96b03ee76e083e6f18f6ca305440fac345579131
Stanislav Lobanov <n10101010@gmail.com>
2014-09-30 20:22:04 +0400

    Added more classifiers

--- setup.py
+++ setup.py
@@ -43,6 +43,11 @@ setup(
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
           'Topic :: Software Development :: Widget Sets',
     ],
 
