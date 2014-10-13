c72bcd97266105af25be1a4cf170e00d16a49147
asyncee <n10101010@gmail.com>
2014-10-13 20:53:54 +0400

    Fix issue #15 "RemovedInDjango18Warning"

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('easy_select2'):
 
 setup(
     name="django-easy-select2",
-    version="1.2.10",
+    version="1.2.11",
     packages=find_packages(),
     author="asyncee",
     description="Django select2 theme for select input widgets.",
