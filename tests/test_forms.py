18b24ebc021061d47a5c3d197b6193197f5096f9
asyncee <asyncee@users.noreply.github.com>
2014-10-18 12:18:16 +0400

    Added forms tests

---
+++ tests/test_forms.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from django import forms
+from easy_select2.forms import FixedModelForm
+from easy_select2.models import TestFieldsModel
+
+
+class TestFixedModelForm(FixedModelForm):
+    class Meta:
+        model = TestFieldsModel
+
+
+def test_fixedmodelform():
+    form = TestFixedModelForm()
+    assert form.fields['fk_field'].help_text == ''
+    assert form.fields['m2m_field'].help_text == ''
