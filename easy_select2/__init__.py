91dec3079928cd4dbdd18309515a4e06572d83c5
Stanislav Lobanov <n10101010@gmail.com>
2015-05-07 19:32:35 +0300

    Cleaned code

--- easy_select2/__init__.py
+++ easy_select2/__init__.py
@@ -1,14 +1,11 @@
 __all__ = [
-    'select2_meta_factory', 'select2_modelform_meta', 'select2_modelform',
+    'select2_modelform_meta', 'select2_modelform',
     'Select2', 'Select2Multiple', 'Select2Mixin', 'apply_select2',
-    'Select2TextMixin', 'Select2TextInput',
 ]
 
 from easy_select2.utils import (
-    select2_meta_factory, select2_modelform_meta, select2_modelform,
-    apply_select2,
+    select2_modelform_meta, select2_modelform, apply_select2,
 )
 from easy_select2.widgets import (
-    Select2Mixin, Select2, Select2Multiple, Select2TextMixin,
-    Select2TextInput,
+    Select2Mixin, Select2, Select2Multiple,
 )
