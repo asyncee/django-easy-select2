1f2eda88d629e7dfa5ff1132e65f7c95b9033727
asyncee <asyncee@users.noreply.github.com>
2015-05-07 19:32:57 +0300

    Removed text field support in sampleproject

--- sampleproject/demoapp/models.py
+++ sampleproject/demoapp/models.py
@@ -15,9 +15,6 @@ class Category(models.Model):
 class Note(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)  # To demonstrate select2
     categories = models.ManyToManyField(Category)  # To demonstrate select2 multiple
-    # To demonstrate select2 text input
-    mood = models.CharField(blank=False, default='', max_length=64,
-                            help_text='What is your current mood?')
 
     def __unicode__(self):
         return u'Note %d' % self.id
