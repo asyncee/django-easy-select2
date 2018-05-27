d350bf69c086879dea06f9f0db14629efedd2ace
gaurav arora <gaurav.arora0403@gmail.com>
2018-05-28 00:35:12 +0530

    issue-44: fixed - mark_safe problem fixed - added select2 plugin constructor argument support - updated easy_select2() wrapper to JQuery plugin

--- easy_select2/static/easy_select2/js/init.js
+++ easy_select2/static/easy_select2/js/init.js
@@ -1,22 +1,4 @@
-(function ($) {
-    "use strict";
-    function redisplay_select2($el){
-        if($("#" + $el.attr('id')).hasClass("select2-hidden-accessible")){
-            $("#" + $el.attr('id')).select2('destroy').select2($el.data());
-        } else{
-            $("#" + $el.attr('id')).select2($el.data());
-        }
-
-    }
-    function add_select2_handlers(){
-        $('div.field-easy-select2:not([id*="__prefix__"])').each(function(){
-            redisplay_select2($(this));
-            $("#" + $(this).attr('id')).on('DOMNodeInserted', function(){
-                $(this).select2('destroy').select2($(this).data());
-            })
-        });
-    }
-    $(function(){
-        add_select2_handlers();
-    });
-}(jQuery || django.jQuery));
+$(document).ready(function(){
+    // easy_select2() can take select2 constructor arguments object
+    $('body').easy_select();
+});
