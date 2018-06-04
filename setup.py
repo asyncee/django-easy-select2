599a3364492538570ab942417011c7ac7ff21142
gaurav arora <gaurav.arora0403@gmail.com>
2018-06-04 13:07:51 +0530

    issue-45: bug fixed  - initializing select2 for tabular and stacked Inlines when no records were added previously

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class Tox(TestCommand):
 
 setup(
     name="django-easy-select2",
-    version="1.5.1",
+    version="1.5.2",
     packages=find_packages(),
     author="asyncee",
     description="Django select2 theme for select input widgets.",
