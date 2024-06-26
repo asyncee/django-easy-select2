68b6b383c33250ac91db5de3d884de513229c297
Owen Miller <owen9825@gmail.com>
2024-06-26 12:57:30 +1000

    Introducing custom search

--- easy_select2/static/easy_select2/js/easy_select2.js
+++ easy_select2/static/easy_select2/js/easy_select2.js
@@ -10,7 +10,6 @@ if (window.$ == undefined) {
     window.$ = _jq;
 }
 
-
 /**
  * $ - external JQuery ref
  * _djq - django Jquery ref
@@ -19,12 +18,40 @@ if (window.$ == undefined) {
     "use strict";
     // store for keeping all the select2 widget ids for fail-safe parsing later
     var _all_easy_select2_ids = [];
+    function myMatcher(params, data) {
+        console.log("Running custom matcher");
+        // If there are no search terms, return all of the data
+        if ($.trim(params.term) === '') {
+          return data;
+        }
+
+        // Do not display the item if there is no 'text' property
+        if (typeof data.text === 'undefined') {
+            console.log('There is no text here');
+          return null;
+        }
+
+        // `params.term` should be the term that is used for searching
+        // `data.text` is the text that is displayed for the data object
+        if (data.text.indexOf(params.term) > -1) {
+          var modifiedData = $.extend({}, data, true);
+          modifiedData.text += ' (matched)';
+
+          // You can return modified objects from here
+          // This includes matching the `children` how you want in nested data sets
+          return modifiedData;
+        }
+
+        // Return `null` if the term should not be displayed
+        return null;
+    }
     /**
      * passing the current element to initialize and the options to initialize with
      * @param $el - Jquery object to initialize with select2
      * @param obj - select2 constructor options
      */
     function redisplay_select2($el, obj){
+        // todo: override the `matcher` argument: https://stackoverflow.com/a/24741027/1495729
         if(!$.fn.select2){
             if(jQuery.fn.select2){
                 $ = jQuery
@@ -33,9 +60,17 @@ if (window.$ == undefined) {
             }
         }
         var $selectEle = $("#" + $el.attr('id'));
+        console.log("Current HTML options for " + $el.attr('id') + ":", $selectEle.html());
+
+        if (obj.use_custom_matcher) {
+            obj.matcher = myMatcher;
+        }
+
         if($selectEle.hasClass("select2-hidden-accessible")){
+            console.log("Re-initializing element");
             $selectEle.select2('destroy').select2(obj);
         } else{
+            console.log("Initializing element");
             $selectEle.select2(obj);
         }
         $selectEle.change(function (e) {
@@ -59,12 +94,14 @@ if (window.$ == undefined) {
 
         $('div.field-easy-select2:not([id*="__prefix__"])').each(function(){
             // taking data-* for select2 constructor properties for backward compatibility
+            // todo: See how all the data attributes get applied to the select2 initialisation. We need a transport here
+            //  https://select2.org/data-sources/ajax#alternative-transport-methods
             var obj = $(this).data();
 
             // merging the options and data properties, modifying the first
             // NOTE: obj properties will be overwritten by options
             // https://api.jquery.com/jquery.extend/
-            $.extend(obj, options);
+            $.extend(obj, options);  // todo: add a custom ajax option here.
             redisplay_select2($(this), obj);
         });
 
